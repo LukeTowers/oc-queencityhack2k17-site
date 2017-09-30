36eebb44494ce7df780e1022d8f832099c131ac4
Ankit Sharma <ankit.sharma@lookagency.com>
2017-09-29 22:52:09 -0600

    Adding Plugins and Form

---
+++ plugins/teamate/ccrezqs/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace TeamAte\Ccrezqs;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
