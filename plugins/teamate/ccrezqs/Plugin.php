fc27d2eec4e53eee9e216c028d8462329efd03af
Luke Towers <luke@luketowers.ca>
2017-09-30 11:22:01 -0600

    moar stuff

--- plugins/teamate/ccrezqs/Plugin.php
+++ plugins/teamate/ccrezqs/Plugin.php
@@ -19,6 +19,8 @@ class Plugin extends PluginBase
             $model->hasMany['dogs'] = 'TeamAte\CcRezqs\Models\Dog';
 
             $model->jsonable(['contact_info', 'residence', 'personal_info', 'animal_info', 'foster_info', 'other_info']);
+
+            $model->addFillable(['contact_info', 'residence', 'personal_info', 'animal_info', 'foster_info', 'other_info']);
         });
 
         UserController::extend(function ($controller) {
@@ -41,5 +43,12 @@ class Plugin extends PluginBase
 
             $form->addTabFields($fields['fields']);
         });
+    }
+
+    public function registerComponents()
+    {
+        return [
+            'TeamAte\Ccrezqs\Components\ApplicationFoster' => 'applicationFoster',
+        ];
     }
 }
---
+++ plugins/teamate/ccrezqs/components/ApplicationFoster.php
@@ -0,0 +1,43 @@
+<?php namespace TeamAte\Ccrezqs\Components;
+
+use Auth;
+use Flash;
+use Cms\Classes\ComponentBase;
+
+class ApplicationFoster extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'ApplicationFoster Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+    public function onApply()
+    {
+        $user = Auth::getUser();
+        if (!$user) {
+            throw new \ApplicationException("You must be logged in to apply!");
+        }
+
+        $input = input();
+
+        try {
+            if (!empty($input)) {
+                $user->fill($input);
+                $user->foster_status = 'applied';
+                $user->save();
+                Flash::success("Your application has been submitted.");
+                return redirect('/foster');
+            }
+        } catch (\Exception $e) {
+            throw new ApplicationException("Something went wrong: " . $e->getMessage());
+        }
+    }
+}
